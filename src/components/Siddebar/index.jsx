20b36429f338232caacdd8c23024ebcc3d922286
Fernanda-Falcao <150362145+Fernanda-Falcao@users.noreply.github.com>
2025-01-07 18:10:54 -0300

    Criação da tela de abono de faltas

--- src/components/Siddebar/index.jsx
+++ src/components/Siddebar/index.jsx
@@ -1,15 +1,59 @@
+import { MaterialSymbol } from 'react-material-symbols';
+import 'react-material-symbols/rounded'; 
+
 import './styles.css'
 
 function Sidebar (){
     return (    
         <nav className="sidebar-nav">
-            <span>Menu </span>
+            <span id='menu-icon-siddebar'>
+                <MaterialSymbol 
+                    icon="Menu"
+                    size={30} 
+                    weight={200}
+                />
+            </span>
             <div className='siddebar-items'>
-                <span><a href="#">Início</a></span>
-                <span><a href="#">Solicitações</a></span>
-                <span><a href="#">Meus dados</a></span>
-                <span><a href="#">Alterar senha</a></span>
-                <span><a href="#">Links úteis</a></span>
+                <span className='items-icons-siddebar'>
+                    <MaterialSymbol
+                        icon='Home'
+                        size={30} 
+                        weight={200}
+                    />
+                    <a href="#">Início</a>
+                </span>
+                <span className='items-icons-siddebar'>
+                    <MaterialSymbol
+                        icon='assignment'
+                        size={30} 
+                        weight={200}
+                    />
+                    <a href="#">Solicitações</a>
+                </span>
+                <span className='items-icons-siddebar'>
+                    <MaterialSymbol
+                        icon='Person'
+                        size={30} 
+                        weight={200}
+                    />
+                    <a href="#">Meus dados</a>
+                </span>
+                <span className='items-icons-siddebar'>
+                    <MaterialSymbol
+                        icon='key'
+                        size={30} 
+                        weight={200}
+                    />
+                    <a href="#">Alterar senha</a>
+                </span>
+                <span className='items-icons-siddebar'>
+                    <MaterialSymbol
+                        icon='link'
+                        size={30} 
+                        weight={200}
+                    />
+                    <a href="#">Links úteis</a>
+                </span>
             </div>
         </nav>      
     )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,79 +1,97 @@
+import { useState } from "react";
 import "./styles.css";
+import { MaterialSymbol } from "react-material-symbols";
 
 function Home() {
+    const [model, setModel] = useState({})
+
     return (
       <div className="container-home">
+        <div className="home-titulo">
+          <h2>Abono de faltas</h2>
+        </div>
         <main>
           <section className="form-section">
             <form>
-              <div className="form-group">
-                <label htmlFor="nome">Nome do aluno(a)</label>
-                <input type="text" id="nome" required />
+              <div className="first-group-fields">
+                <div className="form-group">
+                  <label htmlFor="nome">Nome do aluno(a)</label>
+                  <input type="text" id="nome" required />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="matricula">Número de matrícula</label>
+                  <input type="text" id="matricula" required />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="curso">Curso</label>
+                  <select id="curso" required>
+                    <option value="">Selecione</option>
+                    <option value="curso1">Curso 1</option>
+                    <option value="curso2">Curso 2</option>
+                  </select>
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="periodo">Período/Série</label>
+                  <input type="text" id="periodo" required />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="turno">Turno</label>
+                  <select id="turno" required>
+                    <option value="">Selecione</option>
+                    <option value="manha">Manhã</option>
+                    <option value="tarde">Tarde</option>
+                    <option value="noite">Noite</option>
+                  </select>
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="contato">Telefone/Celular/Email</label>
+                  <input type="text" id="contato" required />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="cpf">CPF</label>
+                  <input type="text" id="cpf" required />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="identidade">Identidade</label>
+                  <input type="text" id="identidade" required />
+                </div>
               </div>
 
-              <div className="form-group">
-                <label htmlFor="matricula">Número de matrícula</label>
-                <input type="text" id="matricula" required />
-              </div>
+              <div className="last-group-fields">
+                  <div className="form-control">
+                    <label htmlFor="anexo">Selecione o tipo de anexo</label>
+                    <select id="select-anexo" required>
+                      <option value="">Selecione</option>
+                      <option value="atestado">Atestado médico</option>
+                    </select>
+                  </div>
 
-              <div className="form-group">
-                <label htmlFor="curso">Curso</label>
-                <select id="curso" required>
-                  <option value="">Selecione</option>
-                  <option value="curso1">Curso 1</option>
-                  <option value="curso2">Curso 2</option>
-                </select>
-              </div>
+                  <div className="form-control">
+                    <label htmlFor="arquivo">Arquivo</label>
+                    <label htmlFor="arquivo" className="custom-file-upload">
+                      <input 
+                        type="file" id="arquivo" 
+                        onChange={(e) => setModel({ ...model, file: e.target.files[0] })}
+                      />
+                      {model.file ? model.file.name : "Nenhum arquivo selecionado"} <MaterialSymbol icon="attach_file" size={20} weight={100}/>
+                    </label>
+                  </div>
 
-              <div className="form-group">
-                <label htmlFor="periodo">Período/Série</label>
-                <input type="text" id="periodo" required />
+                  <div className="form-control">
+                    <label htmlFor="observacoes">Observações</label>
+                    <textarea id="observacoes" rows="4"></textarea>
+                  </div>
               </div>
+              
 
-              <div className="form-group">
-                <label htmlFor="turno">Turno</label>
-                <select id="turno" required>
-                  <option value="">Selecione</option>
-                  <option value="manha">Manhã</option>
-                  <option value="tarde">Tarde</option>
-                  <option value="noite">Noite</option>
-                </select>
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="contato">Telefone/Celular/Email</label>
-                <input type="text" id="contato" required />
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="cpf">CPF</label>
-                <input type="text" id="cpf" required />
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="identidade">Identidade</label>
-                <input type="text" id="identidade" required />
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="anexo">Selecione o tipo de anexo</label>
-                <select id="anexo" required>
-                  <option value="">Selecione</option>
-                  <option value="atestado">Atestado médico</option>
-                </select>
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="arquivo">Arquivo</label>
-                <input type="file" id="arquivo" required />
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="observacoes">Observações</label>
-                <textarea id="observacoes" rows="4"></textarea>
-              </div>
-
-              <button type="submit" className="submit-btn">
+              <button type="submit" className="submit-btn-abono">
                 Solicitar
               </button>
             </form>
